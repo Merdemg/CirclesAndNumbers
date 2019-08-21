856bc3d19e090c1390381a057f031dac8839b464
Erdem Gunay <merdemgunay88@gmail.com>
2019-08-20 22:27:28 -0400

    rotation

--- Assets/incomingCircle.cs
+++ Assets/incomingCircle.cs
@@ -7,6 +7,12 @@ public class incomingCircle : MonoBehaviour
     bubble[] myBubbles;
     [SerializeField] float speed = 1.1f;
 
+    bool isRotating = false;
+    const float ROTATION_SPEED = 1f;
+    [SerializeField] float targetAngle = 0;
+    float counter = 0;
+    float prevAngle = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,24 @@ public class incomingCircle : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        fakeMovement();
+
+        if (isRotating)
+        {
+            counter += Time.deltaTime * ROTATION_SPEED;
+            if (counter >= 1)
+            {
+                counter = 1;
+                isRotating = false;
+            }
+
+            float angle = Mathf.LerpAngle(prevAngle, targetAngle, counter);
+            transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+
+    void fakeMovement()
     {
         transform.localScale = transform.localScale + (transform.localScale * Time.deltaTime * speed);
 
@@ -29,7 +53,38 @@ public class incomingCircle : MonoBehaviour
         return myBubbles;
     }
 
-    public void RotateLeft()
+    void RotateLeft()
+    {
+        counter = 0;
+        prevAngle = this.transform.eulerAngles.z;
+        targetAngle += 45;
+        FixTargetAngle();
+        isRotating = true;
+    }
+
+    void RotateRight()
+    {
+        counter = 0;
+        prevAngle = this.transform.eulerAngles.z;
+        targetAngle += 45;
+        FixTargetAngle();
+        isRotating = true;
+    }
+
+    void FixTargetAngle()
+    {
+        while (targetAngle >= 360)
+        {
+            targetAngle -= 360;
+        }
+
+        while (targetAngle < 0)
+        {
+            targetAngle += 360;
+        }
+    }
+
+    public void RotateArrayLeft()
     {
         bubble temp = myBubbles[0];
         for (int i = 0; i < myBubbles.Length - 1; i++)
@@ -39,7 +94,7 @@ public class incomingCircle : MonoBehaviour
         myBubbles[myBubbles.Length - 1] = temp;
     }
 
-    public void RotateRight()
+    public void RotateArrayRight()
     {
         bubble temp = myBubbles[myBubbles.Length - 1];
         for (int i = myBubbles.Length - 1; i > 0; i--)
